bf653464d854b390e9801ca4c81403359588a288
zhx <zhouhuixiang@vswork.com>
2024-07-24 16:25:48 +0800

    ++

--- src/routes/(login)/demo/+page.ts
+++ src/routes/(login)/demo/+page.ts
@@ -6,3 +6,4 @@ export const csr = dev
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true
+console.log(1)
